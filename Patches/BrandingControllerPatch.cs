c2c825fa25f169db11670cb2ba4f706dbe410d84
Christian Kosman <christiankosman@gmail.com>
2022-09-27 20:19:40 -0700

    Initial commit

---
+++ Patches/BrandingControllerPatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SkipIntro
+{
+    [HarmonyPatch(typeof(BrandingController), "Start")]
+    internal class BrandingControllerPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix() 
+        {
+            SceneManager.LoadScene("saveslot");
+        }
+    }
+}
---
+++ Patches/SaveSlotControllerPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace SkipIntro.Patches
+{
+    [HarmonyPatch(typeof(SaveSlotController), "Start")]
+    internal class SaveSlotControllerPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(SaveSlotController __instance)
+        {
+            //__instance.clickBtn1();
+            SaverLoader.global_saveindex = 0;
+            SaverLoader.loadSavedGame();
+            MethodInfo mi = __instance.GetType().GetMethod("checkScores", BindingFlags.NonPublic | BindingFlags.Instance);
+            mi.Invoke(__instance, null);
+            AchievementSetter.checkAllCheevos();
+            SceneManager.LoadScene("home");
+        }
+    }
+}
